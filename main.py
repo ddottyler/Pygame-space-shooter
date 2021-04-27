fc9d8a1c8b250db9175e5aea12027615aff10ed8
Dan T <dp.tyler@hotmail.co.uk>
2021-04-27 21:07:51 +0100

    Player and laser class created

--- main.py
+++ main.py
@@ -48,6 +48,48 @@ class Player:
         elif self.cool_down_counter > 0:
             self.cool_down_counter += 1
 
+    def move_lasers(self, vel, obj):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -= REDUCE_HEALTH
+                self.lasers.remove(laser)
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x, self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
+
+    def get_width(self):
+        return self.player_img.get_width()
+
+    def get_height(self):
+        return self.player_img.get_height()
+
+
+class Laser:
+    def __init__(self, x, y, img)
+    self.x = x
+    self.y = y
+    self.img = img
+    self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return not(self.y <= height and self.y >= 0)
+
+    def collision(self, obj):
+        return collide(obj, self)
+
 
 def main():
     run = True
