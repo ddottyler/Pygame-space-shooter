93bc58a744ac3863402187f736d5002c70f93310
Dan T <dp.tyler@hotmail.co.uk>
2021-04-28 10:14:42 +0100

    Player 2 class added

--- main.py
+++ main.py
@@ -48,18 +48,15 @@ class Player:
         elif self.cool_down_counter > 0:
             self.cool_down_counter += 1
 
-    def move_lasers(self, vel, objs):
+    def move_lasers(self, vel, obj):
         self.cooldown()
         for laser in self.lasers:
             laser.move(vel)
             if laser.off_screen(HEIGHT):
                 self.lasers.remove(laser)
-            else:
-                for obj in objs:
-                    if laser.collision(obj):
-                        objs.remove(obj)
-                        if laser in self.lasers:
-                            self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -= REDUCE_HEALTH
+                self.lasers.remove(laser)
 
     def shoot(self):
         if self.cool_down_counter == 0:
@@ -79,6 +76,25 @@ class Player_1(Player):
         super().__init__(x, y, health)
         self.player_img = PLAYER_1
         self.laser_img = PLAYER_1_LASER
+        self.mask = pygame.mask.from_surface(self.player_img)
+        self.max_health = health
+
+    def draw(self, window):
+        super().draw(window)
+        self.healthbar(window)
+
+    def healthbar(self, window):
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y +
+                         self.player_img.get_height() + 10, self.player_img.get_width(), 10))
+        pygame.draw.rect(window, (0, 255, 0), (self.x, self.y + self.player_img.get_height() +
+                         10, self.player_img.get_width() * (self.health/self.max_health), 10))
+
+
+class Player_2(Player):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.player_img = PLAYER_2
+        self.laser_img = PLAYER_2_LASER
         self.mask = pygame.mask.from_surface(self.player_img)
         self.max_health = health
 
